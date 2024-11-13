1ab7b0daafd041293dd93ef652cfb4c65205d10b
Grzegorz Kozera <01171661@pw.edu.pl>
2024-11-13 23:38:24 +0100

    zej1

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,20 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    if (towar=0){ return 0; }
+    unsigned int trans_towar;
+    unsigned int count;
+    
+    Stocznia stocznia{};
+
+    while(trans_towar < towar){
+        Statek* statek = stocznia();
+        unsigned int capacity = statek->transportuj();
+        trans_towar += capacity;
+        delete statek;
+
+        if(dynamic_cast<Zaglowiec*>(statek)){ count++; }
+    }
+
+    return count;
 }

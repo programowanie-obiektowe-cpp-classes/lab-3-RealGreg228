aa23a47417c34bfc70a1d28897f390476c24174f
Grzegorz Kozera <01171661@pw.edu.pl>
2024-11-14 14:00:30 +0100

    zej3.2

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -8,21 +8,24 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    if (towar=0){ return 0; }
-    unsigned int trans_towar = 0;
+    if (towar == 0){
+        return 0;
+    }
+    unsigned int trans = 0;
     unsigned int count = 0;
-    
     Stocznia stocznia;
 
-    while(trans_towar < towar){
+    while (trans < towar) {
         Statek* statek = stocznia();
-        unsigned int capacity = statek->transportuj();
-        trans_towar += capacity;
-        
-        if(dynamic_cast<Zaglowiec*>(statek) != nullptr){ 
-            count++; 
+        unsigned int amount = statek->transportuj();
+        trans += amount;
+
+        // SprawdŸ, czy statek jest typu Zaglowiec
+        if (dynamic_cast< Zaglowiec* >(statek) != nullptr) {
+            count++;
         }
-        delete statek;
+
+        delete statek; // Usuñ dynamicznie utworzony statek
     }
 
     return count;
